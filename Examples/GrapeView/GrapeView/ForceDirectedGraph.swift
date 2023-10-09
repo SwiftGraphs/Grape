dca28dfb6b77af5f1e8900cb8c0f84460b2e71fc
li3zhen1 <master_lee@live.com>
2023-10-09 00:49:16 -0400

    update example

---
+++ Examples/GrapeView/GrapeView/ForceDirectedGraph.swift
@@ -0,0 +1,18 @@
+//
+//  ForceDirectedGraph.swift
+//  GrapeView
+//
+//  Created by li3zhen1 on 10/8/23.
+//
+
+import Foundation
+
+
+import SwiftUI
+import ForceSimulation
+
+
+
+struct ForceDirectedGraph {
+    
+}
---
+++ Examples/GrapeView/GrapeView/GrapeViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  GrapeViewApp.swift
+//  GrapeView
+//
+//  Created by li3zhen1 on 10/8/23.
+//
+
+import SwiftUI
+
+@main
+struct GrapeViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
