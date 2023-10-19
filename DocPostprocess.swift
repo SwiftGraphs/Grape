9a1fb04846e69c9f99cacf3464872e22b5bad2b9
li3zhen1 <master_lee@live.com>
2023-10-19 19:18:12 -0400

    Fix Doc icon

--- DocPostprocess.swift
+++ DocPostprocess.swift
@@ -18,7 +18,6 @@ do {
     htmlString = htmlString.replacingOccurrences(of: """
     <link rel="mask-icon" href="/Grape/favicon.svg" color="#333333">
     """, with: """
-<link rel="mask-icon" href="/Grape/favicon.png" color="#333333">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
