06ccfeb6b2b8e988dd8efc5df23eef00aa5331ac
li3zhen1 <master_lee@live.com>
2023-11-03 20:35:38 -0400

    Pass compiler

--- Sources/ForceSimulation/ForceLike.swift
+++ Sources/ForceSimulation/ForceLike.swift
@@ -5,6 +5,8 @@
 //  Created by li3zhen1 on 10/1/23.
 //
 
+import simd
+
 /// A protocol that represents a force.
 /// A force takes a simulation state and modifies its node positions and velocities.
 public protocol ForceLike {
@@ -18,6 +20,8 @@ public protocol NDTreeBasedForceLike: ForceLike {
     associatedtype TD: NDTreeDelegate
 }
 
+// extension Simulation {
+
 @resultBuilder
 public struct ForceFieldBuilder {
     public static func buildBlock<each F>(_ forces: repeat each F) -> (repeat each F) {
@@ -25,7 +29,10 @@ public struct ForceFieldBuilder {
     }
 }
 
-public struct ForceField<each F> where repeat each F: ForceLike {
+public struct ForceField< /*NodeID, V, */each F>
+where
+    repeat each F: ForceLike  //, NodeID: Hashable, V: VectorLike, V.Scalar: SimulatableFloatingPoint
+{
 
     @usableFromInline let forces: (repeat each F)
 
@@ -46,12 +53,53 @@ public struct ForceField<each F> where repeat each F: ForceLike {
     @inlinable func apply() {
         repeat (each forces).apply()
     }
+
+    // public typealias SIM = SimulationKD<NodeID, V>
+
+    // @discardableResult
+    // @inlinable
+    // public func withF1() -> ForceField<
+    //     NodeID, V, repeat each F, F1
+    // > {
+    //     return ForceField<NodeID, V, repeat each F, F1>(
+    //         forces: repeat each forces,
+    //         F1()
+    //     )
+    // }
 }
 
-extension ForceLike {
-    @inlinable public func packWith<each F>(_ pack: ForceField<repeat each F>) -> ForceField<
-        repeat each F, Self
-    > where repeat each F: ForceLike {
-        return pack.append(self)
+// }
+
+// extension ForceLike {
+//     @inlinable public func packWith<each F>(_ pack: Simulation.ForceField<repeat each F>) -> ForceField<
+//         repeat each F, Self
+//     > where repeat each F: ForceLike {
+//         let result = pack.append(self)
+//         return result
+//     }
+// }
+
+public struct F1: ForceLike {
+    @inlinable public func apply() {}
+}
+
+struct SimulationBuilder<NodeID, V, each Force>
+where
+    NodeID: Hashable, V: VectorLike, V.Scalar: SimulatableFloatingPoint,
+    repeat each Force: ForceLike
+{
+    let forces: ForceField<repeat each Force>
+
+    public typealias Sim = SimulationKD<NodeID, V>
+
+    @inlinable init(forces: ForceField<repeat each Force>) {
+        self.forces = forces
+    }
+
+    func withF1() -> SimulationBuilder<NodeID, V, repeat each Force, Sim.CenterForce> {
+        let newForceField = self.forces.append(
+            Sim.CenterForce(center: .zero, strength: 1)
+        )
+        return SimulationBuilder<NodeID, V, repeat each Force, Sim.CenterForce>(forces: newForceField)
     }
 }
--- Sources/ForceSimulation/Forces/Simulation.CenterForce.swift
+++ Sources/ForceSimulation/Forces/Simulation.CenterForce.swift
@@ -56,4 +56,5 @@ extension SimulationKD {
         return f
     }
 
+
 }
