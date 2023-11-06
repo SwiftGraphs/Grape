c08ea8508af39d3fca833e172aafb9da2429717a
li3zhen1 <master_lee@live.com>
2023-11-06 09:29:23 -0500

    Update docc process

--- DocPostprocess.swift
+++ DocPostprocess.swift
@@ -3,7 +3,11 @@ import Foundation
 // Define the paths for the files
 let docsDirectoryPath = "./docs"
 let iconSourcePath = "./assets/grape_icon_256.png"
-let iconDestPath = "./docs/favicon.png" 
+let iconDestPath = "./docs/favicon.png"
+let moduleNames = [
+    "Grape",
+    "ForceSimulation",
+]
 
 do {
     let fileManager = FileManager.default
@@ -17,42 +21,45 @@ do {
 
             while let element = enumerator?.nextObject() as? String {
                 if element.hasSuffix("index.html") {  // checks the extension
+                    print(element)
                     let indexPath = "\(docsDirectoryPath)/\(element)"
                     var htmlString = try String(contentsOfFile: indexPath, encoding: .utf8)
 
-                    htmlString = htmlString.replacingOccurrences(
-                        of: """
-                            <link rel="icon" href="/Grape/favicon.ico">
-                            """,
-                        with: """
-                            <link rel="icon" href="/Grape/favicon.png">
-                            """)
-                    htmlString = htmlString.replacingOccurrences(
-                        of: """
-                            <link rel="mask-icon" href="/Grape/favicon.svg" color="#333333">
-                            """,
-                        with: """
-                            <link rel="preconnect" href="https://fonts.googleapis.com">
-                            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-                            <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
-                            <link rel="stylesheet" media="all" href="https://lizhen.me/inter/inter.css" type="text/css"/>
-                            <style>
-                                :root {
-                                    --typography-html-font: "intervar";
-                                    --typography-html-font-mono: "SF Mono", ui-monospace, "JetBrains Mono";
-                                }
-                                h1.title {
-                                    font-weight: 600!important;
-                                    font-variation-settings: 'wght' 600, 'opsz' 24!important;
-                                }
-                                h2 {
-                                    font-weight: 600!important;
-                                    font-variation-settings: 'wght' 600, 'opsz' 24!important;
-                                }
-                            </style>
-                            
-                            """)
+                    for moduleName in moduleNames {
+                        htmlString = htmlString.replacingOccurrences(
+                            of: """
+                                <link rel="icon" href="/Grape/\(moduleName)/favicon.ico">
+                                """,
+                            with: """
+                                <link rel="icon" href="/Grape/\(moduleName)/favicon.png">
+                                """)
 
+                        htmlString = htmlString.replacingOccurrences(
+                            of: """
+                                <link rel="mask-icon" href="/Grape/\(moduleName)/favicon.svg" color="#333333">
+                                """,
+                            with: """
+                                <link rel="preconnect" href="https://fonts.googleapis.com">
+                                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+                                <link href="https://fonts.googleapis.com/css2?family=JetBrains+Mono:ital,wght@0,400;0,600;1,400;1,600&display=swap" rel="stylesheet">
+                                <link rel="stylesheet" media="all" href="https://lizhen.me/inter/inter.css" type="text/css"/>
+                                <style>
+                                    :root {
+                                        --typography-html-font: "intervar";
+                                        --typography-html-font-mono: "SF Mono", ui-monospace, "JetBrains Mono";
+                                    }
+                                    h1.title {
+                                        font-weight: 600!important;
+                                        font-variation-settings: 'wght' 600, 'opsz' 24!important;
+                                    }
+                                    h2 {
+                                        font-weight: 600!important;
+                                        font-variation-settings: 'wght' 600, 'opsz' 24!important;
+                                    }
+                                </style>
+
+                                """)
+                    }
                     try htmlString.write(toFile: indexPath, atomically: false, encoding: .utf8)
                 }
             }
