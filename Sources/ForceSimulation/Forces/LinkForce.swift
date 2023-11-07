076b68315149f85e36aa65222339b1fc52874f52
li3zhen1 <master_lee@live.com>
2023-11-07 17:28:41 -0500

    Moving POP

--- Sources/ForceSimulation/Forces/LinkForce.swift
+++ Sources/ForceSimulation/Forces/LinkForce.swift
@@ -1,5 +1,9 @@
 extension Kinetics {
+
     public final class LinkForce: ForceProtocol {
+        @usableFromInline
+        internal var kinetics: Kinetics! = nil
+
         @inlinable
         public func apply() {
 
@@ -8,9 +12,17 @@ extension Kinetics {
         @usableFromInline
         internal var links: [EdgeID<Int>]
 
+        @usableFromInline
+        internal var linkLookup: LinkLookup<Int> = .init(links: [])
+
         @inlinable
         public func bindKinetics(_ kinetics: Kinetics) {
-
+            self.kinetics = kinetics
+            self.linkLookup = .init(
+                links: self.links.filter {
+                    $0.source < kinetics.validCount && $0.target < kinetics.validCount
+                }
+            )
         }
 
         @inlinable
@@ -18,4 +30,22 @@ extension Kinetics {
             self.links = links
         }
     }
+}
+
+public struct LinkLookup<NodeID: Hashable> {
+    public let sourceToTarget: [NodeID: [NodeID]]
+    public let targetToSource: [NodeID: [NodeID]]
+
+    @inlinable
+    public init(links: [EdgeID<NodeID>]) {
+        var sourceToTarget: [NodeID: [NodeID]] = [:]
+        var targetToSource: [NodeID: [NodeID]] = [:]
+        for link in links {
+            sourceToTarget[link.source, default: []].append(link.target)
+            targetToSource[link.target, default: []].append(link.source)
+        }
+        self.sourceToTarget = sourceToTarget
+        self.targetToSource = targetToSource
+    }
+
 }
