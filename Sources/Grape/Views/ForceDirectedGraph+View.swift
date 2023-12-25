d3cbe9d67d0887a14404051bedf8d6dc22ad435a
Li, Zhen <zhenli.craig@gatech.edu>
2023-12-24 23:15:11 -0500

    Add states

--- Sources/Grape/Views/ForceDirectedGraph+View.swift
+++ Sources/Grape/Views/ForceDirectedGraph+View.swift
@@ -4,21 +4,32 @@ extension ForceDirectedGraph: View {
     public var body: some View {
         #if DEBUG
         let _ = Self._printChanges()
-        let _ = print("Evaluating View: \(self.graphContext.nodes.count)")
+        let _ = print("Evaluating View: \(self.model.graphRenderingContext.nodes.count)")
         #endif
         VStack {
+            Text("Elapsed Time: \(model.currentFrame.rawValue)")
             Button {
                 self.clickCount += 1
             } label: {
                 Text("Click \(clickCount)")
-                Text(changeMessage)
+                Text(self.model.changeMessage)
             }
-            ForEach(self.graphContext.nodes, id: \.id) { node in
+            ForEach(self.model.graphRenderingContext.nodes, id: \.id) { node in
                 Text("\(node.debugDescription)")
             }
-        }.onChange(of: self.graphContext, initial: true) { oldValue, newValue in
-            print("Graph context changed from \(oldValue.nodes.count) to \(newValue.nodes.count)")
-            self.changeMessage = "Graph context changed from \(oldValue.nodes.count) to \(newValue.nodes.count)"
+        }
+        .onChange(
+            of: self._graphRenderingContextShadow, 
+            initial: false // Don't trigger on initial value, keep `changeMessage` as "N/A"
+        ) { _, newValue in
+            self.model.revive(with: newValue)
+        }
+        .onChange(of: self.isRunning, initial: false) { oldValue, newValue in
+            if newValue {
+                self.model.start()
+            } else {
+                self.model.stop()
+            }
         }
     }
 }
--- Sources/Grape/Views/ForceDirectedGraph.swift
+++ Sources/Grape/Views/ForceDirectedGraph.swift
@@ -8,27 +8,144 @@ class GCRBox<NodeID: Hashable> {
     }
 }
 
+public struct KeyFrame {
+    public var elapsed: UInt = 0
 
-class ForceDirectedGraphModel<NodeID: Hashable>: ObservableObject {
-    init() {
+    @_transparent
+    public init(rawValue: UInt) {
+        self.elapsed = rawValue
+    }
 
+    @_transparent
+    public mutating func advance(by delta: UInt = 1) {
+        elapsed += delta
+    }
+
+    @_transparent
+    public mutating func reset() {
+        elapsed = 0
     }
 }
 
-public struct ForceDirectedGraph<NodeID: Hashable> {
+extension KeyFrame: RawRepresentable, Equatable, Hashable, ExpressibleByIntegerLiteral {
 
-    var graphContext: _GraphRenderingContext<NodeID>
+    @_transparent
+    public var rawValue: UInt {
+        return elapsed
+    }
+
+    @_transparent
+    public init(integerLiteral value: UInt) {
+        self.init(rawValue: value)
+    }
+}
+
+@Observable
+final class ForceDirectedGraphModel<NodeID: Hashable> {
+    @ObservationIgnored
+    var graphRenderingContext: _GraphRenderingContext<NodeID>
+
+    var changeMessage = "N/A"
+
+    // @ObservationIgnored
+    var currentFrame: KeyFrame = 0
+
+    @ObservationIgnored
+    let ticksPerSecond: Double
+
+    @ObservationIgnored
+    var scheduledTimer: Timer? = nil
+
+    init(
+        _ graphRenderingContext: _GraphRenderingContext<NodeID>,
+        ticksPerSecond: Double = 60.0
+    ) {
+        self.graphRenderingContext = graphRenderingContext
+        self.ticksPerSecond = ticksPerSecond
+
+        // let timer = Timer.scheduledTimer(withTimeInterval: 0.1, repeats: true) {
+        //     [weak self] timer in
+        //     guard let self = self else {
+        //         timer.invalidate()
+        //         return
+        //     }
+        //     self.elapsedTime += 0.1
+        // }
+
+        // RunLoop.main.add(timer, forMode: .common)
+    }
+
+    func start() {
+        guard self.scheduledTimer == nil else { return }
+        self.scheduledTimer = Timer.scheduledTimer(
+            withTimeInterval: 1.0 / ticksPerSecond,
+            repeats: true
+        ) { [weak self] _ in
+            self?.tick()
+        }
+    }
+
+    func tick() {
+        withMutation(keyPath: \.currentFrame) {
+            currentFrame.advance()
+        }
+    }
+
+    func stop() {
+        self.scheduledTimer?.invalidate()
+        self.scheduledTimer = nil
+    }
+
+    deinit {
+        stop()
+    }
+
+    func revive(with newContext: _GraphRenderingContext<NodeID>) {
+        self.changeMessage =
+            "graphRenderingContext update: \(graphRenderingContext.nodes.count) nodes -> \(newContext.nodes.count) nodes"
+        self.graphRenderingContext = newContext
+    }
+
+}
+
+public struct ForceDirectedGraph<NodeID: Hashable> {
+ 
+    // the copy of the graph context to be used for comparison in `onChange`
+    // should be not used for rendering
+    let _graphRenderingContextShadow: _GraphRenderingContext<NodeID> 
 
     // Some state to be retained when the graph is updated
-    @State var clickCount = 0
+    @State 
+    var clickCount = 0
 
-    @State var changeMessage = "N/A"
+    @State
+    var model: ForceDirectedGraphModel<NodeID>
+    
+    @Binding
+    var isRunning: Bool
 
     public init(
+        _ isRunning: Binding<Bool> = .constant(true),
         @GraphContentBuilder<NodeID> _ graph: () -> some GraphContent<NodeID>
     ) {
         var gctx = _GraphRenderingContext<NodeID>()
         graph()._attachToGraphRenderingContext(&gctx)
-        self.graphContext = gctx
+        self._graphRenderingContextShadow = gctx
+
+
+        // self._isRunning = isRunning
+        self._isRunning = isRunning
+
+
+
+        self.model = ForceDirectedGraphModel(gctx)
+
+    }
+}
+
+
+extension ForceDirectedGraph: Equatable {
+    public static func == (lhs: ForceDirectedGraph<NodeID>, rhs: ForceDirectedGraph<NodeID>) -> Bool {
+        return lhs._graphRenderingContextShadow == rhs._graphRenderingContextShadow
     }
 }
