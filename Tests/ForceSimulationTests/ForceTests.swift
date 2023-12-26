7d5660f01ecd896585655ee2f6a7fba13627788f
Li, Zhen <zhenli.craig@gatech.edu>
2023-12-26 17:33:14 -0500

    Add force tests

--- Tests/ForceSimulationTests/ForceTests.swift
+++ Tests/ForceSimulationTests/ForceTests.swift
@@ -12,26 +12,49 @@ import simd
 
 final class ForceTests: XCTestCase {
 
-    func testManybodyForceMutatePositions() {
-
-        let myForce = Kinetics2D.ManyBodyForce(strength: -300)
-
+    private func _testForceMutatePositions(_ myForce: some Force2D) {
 
         let simulation = Simulation(
             nodeCount: 5,
-            links: [],
+            links: [(0, 1), (1, 2), (2, 3), (3, 4), (4, 0)].map { 
+                EdgeID(source: $0.0, target: $0.1)
+            },
             forceField: myForce
         )
 
-        for i in 0...1000 {
+        for i in 0...10 {
             simulation.tick()
         }
 
         let position = simulation.kinetics.position.asArray()
 
-        print(position)
 
         XCTAssertNotEqual(position, Array(repeating: .zero, count: 5))
+    }
+
+
+    func testLinkForceMutatesPosition() {
+        _testForceMutatePositions(
+            SealedForce2D {
+                Kinetics2D.LinkForce(
+                    stiffness: .weightedByDegree(k: { _, _ in 1.0 }),
+                    originalLength: .constant(35)
+                )
+            }
+        )
+    }
+
+
+    func testManyBodyForceMutatesPosition() {
+        _testForceMutatePositions(
+            SealedForce2D {
+                Kinetics2D.LinkForce(
+                    stiffness: .weightedByDegree(k: { _, _ in 1.0 }),
+                    originalLength: .constant(35)
+                )
+                Kinetics2D.ManyBodyForce(strength: -300)
+            }
+        )
     }
 
 }
