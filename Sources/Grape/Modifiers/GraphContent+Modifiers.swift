9e938cdf28883803aaf7fec03e1b81cd201e2d53
Li, Zhen <zhenli.craig@gatech.edu>
2023-12-26 23:18:43 -0500

    Checkpoint: Add shading tests

--- Sources/Grape/Modifiers/GraphContent+Modifiers.swift
+++ Sources/Grape/Modifiers/GraphContent+Modifiers.swift
@@ -2,9 +2,8 @@ import SwiftUI
 extension GraphContent {
     @inlinable
     public func foregroundStyle<S>(_ style: S) -> some GraphContent<NodeID> where S: ShapeStyle {
-        return ModifiedGraphContent(self, GrapeEffect.ForegroundStyle(style))
+        return ModifiedGraphContent(self, GrapeEffect.Shading(.style(style)))
     }
-
 
     @inlinable
     public func fill(_ value: GraphicsContext.Shading) -> some GraphContent<NodeID> {
