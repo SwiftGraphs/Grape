ab36a8947b46e120dcb73142db95cf038573c926
Zhen Li <craig.lizhen@outlook.com>
2024-10-07 13:11:23 -0400

    [DocC] Update

--- Package.swift
+++ Package.swift
@@ -27,7 +27,7 @@ let package = Package(
     ],
 
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.3.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3")
     ],
 
     targets: [
